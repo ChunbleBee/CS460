f122c2cf6dd1cb76cf45248f080715eaedd8ba73
ChunbleBee <https://ChunbleBee@github.com>
2020-09-29 17:21:16 -0700

    Implemented kexit

---
+++ Lab04/queue.c
@@ -0,0 +1,38 @@
+/************************  queue.c file ************************
+1. Write your C code for the functions specified below
+2. Compile queue.c into .o file by
+      arm-none-eabi-gcc -c -mcpu=arm926ej-s queue.c -o queue.o
+3. Use it in the LINK step by
+      arm-none-eabi-ld -T t.ld ts.o t.o   queue.o   -o t.elf
+****************************************************************/
+
+//#include "type.h"
+extern PROC *freeList, *readyQueue, *running;
+
+// write YOUR C cdoe for the following functions
+
+PROC *get_proc(PROC **list)  // called as get_proc(&freeList)
+{
+  // return a free PROC from freeList
+}
+   
+int put_proc(PROC **list, PROC *p)   // called as put_proc(&feeList, p)
+{
+  // enter PROC p to end of freeList
+}
+
+int enqueue(PROC **queue, PROC *p)   // called as enqueue(&readyQueue, p)
+{
+  // enter p into readyQueue by priority; FIFO if same priority
+}
+
+PROC *dequeue(PROC **queue)         // called as dequeue(&readyQueue)
+{
+  // remove and return FIRST entry from queue
+}
+
+int printList(char *listname, PROC *list)
+{
+  // print link list as      listname = ...... NULL
+}
+
---
+++ Lab04/wait.c
@@ -0,0 +1,67 @@
+//#include "type.h"
+extern PROC *freeList, *readyQueue, *running, *proc;
+
+/************ wait.c file ****************/
+int ksleep(int event)
+{
+    int CPSRRegValue = int_off();   // Turn interrupts off.
+    running->event = event;         // Set the event to wake for the running process
+    running->status = SLEEP;        // Set the status of running process to sleeping
+    tswitch();                      // Switch running process
+    in_on(CPSRRegValue);            // Turn interrupts back on.
+}
+
+int kwakeup(int event)
+{
+    int CPSRRegValue = int_off();   // Turn interrupts off
+    for (u32 i = 0; i < NPROC; i++)
+    {
+        PROC * p = &proc[i];
+        if (p->status == SLEEP && p->event == event)
+        {
+            p->status = READY;
+            enqueue(&readyQueue, p);
+        }
+    }
+    int_on(CPSRRegValue);
+}
+
+int kexit(int exitValue)
+{
+    // 1. Erase process user-mode context values, e.g., close descriptors, release resources, etc.
+    // 2. Dispose of children processes, i.e., give them to the p1 process
+    // 3. record exitValue in process exitcode
+    // 4. Become a ZOMBIE process
+    // 5. Wakeup parent process, and, if necessary, the init process in P1
+    // 6. Switch processes.
+    Proc * p1           = &proc[1];
+    Proc * p1Child      = p1->child;
+    Proc * curOrphan    = running->child;
+    Proc * prevOrphan   = NULL;
+
+    int CPSRRegValue = int_off();
+
+    if (curOrphan != NULL)
+    {
+        while (curOrphan != NULL)
+        {
+            curOrphan->parent   = p1;
+            prevOrphan          = curOrphan;
+            curOrphan           = curOrphan->sibling;
+        }
+        prevOrphan->sibling = p1Child;
+        p1->child           = running->child;
+        running->child      = NULL;
+    }
+
+    running->exitCode   = exitValue;
+    running->status     = ZOMBIE;
+    kwakeup(running->parent);
+    tswitch();
+    int_on(CPSRRegValue);
+}
+
+int kwait(int *status)
+{
+  // kwait function as in class notes
+}
